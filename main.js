e5bde751a78e38571ad53f670be929cd7cddf3c5
Doeun Kim <doeun.kim@emotion.co.kr>
2022-02-24 17:58:08 +0900

    ing

--- main.js
+++ main.js
@@ -1,28 +1,15 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-const canvas2 = document.getElementById('canvas2');
-const ctx2 = canvas2.getContext('2d');
 
-canvas.width = window.innerWidth - 100;
-canvas.height = window.innerHeight - 100;
-canvas2.width = window.innerWidth - 100;
-canvas2.height = window.innerHeight - 100;
-
-function drawBg(){
-    var background = new Image();
-    background.src = 'bg.png';
-    
-    background.addEventListener('load', function(){
-        ctx2.drawImage(background, 0, 0);
-    }, false)
-}
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 var dinoImg = new Image();
 dinoImg.src = 'mario.png';
 
 var dino = {
     x: 10,
-    y: 200,
+    y: 400,
     width: 50,
     height: 50,
     draw(){
@@ -30,90 +17,159 @@ var dino = {
     }
 }
 
+var cactusImg = new Image();
+cactusImg.src = 'item.jpg';
+
 class Cactus{
     constructor(){
-        this.x = 500;
-        this.y = 200;
+        this.x = canvas.width;
+        this.y = 400;
         this.width = 50;
         this.height = 50;
     }
     draw(){
         ctx.fillStyle = 'coral';
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(cactusImg, this.x, this.y, this.width, this.height);
     }
 }
 
-window.onload = function(){
-    drawBg();
-    gameState.startGame();
-}
 
-var animation;
-var gameState = {
-    timer: 0,
-    cactusArray: [],
-    jumping: false,
-    jumpTimer: 0,
-    isEnd: false,
-    startGame: function game(){
-        this.timer++;
-    
+var dinoPosition;
+var cactusPosition;
+var diffPosition;
+var animation,
+    dieAnimation,
+    timer = 0,
+    cactusArray = [],
+    jumping = false,
+    jumpTimer = 0,
+    dying = false,
+    dieTimer = 0,
+    isEnd = false,
+    speed = 5;
+    random = 0;
+
+var gameState = function(){
+    function startGame(){
+        scorePosition = `내 점수 : ${timer}`
+        document.getElementById('score').innerHTML = scorePosition;
+
+        dinoPosition = `나의 위치 x : ${dino.x + dino.width}, y : ${dino.y + dino.height}`
+        document.getElementById('tit2').innerHTML = dinoPosition;
+
+        timer += speed;
+        random = (Math.floor(Math.random() * 10) + 5) * 60;
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        animation = requestAnimationFrame(startGame);
     
-        if (this.timer % 180 === 0) {
+        if (timer % random === 0) {
             var cactus = new Cactus();
-            this.cactusArray.push(cactus);
+            cactusArray.push(cactus);
         }
-        this.cactusArray.forEach((cactus, index, obj) => {
-            if (cactus.x < 0) {
+        cactusArray.forEach((cactus, index, obj) => {
+            if (cactus.x < - cactus.width) {
                 obj.splice(index, 1);
             }
     
-            this.isCrashed(dino, cactus);
+            isCrashed(dino, cactus);
     
-            cactus.x-=2;
+            cactus.x -= speed * 2;
             cactus.draw();
-        })
+            
+            if (index === 0) {
+                cactusPosition = `장애물의 위치 x : ${cactus.x + cactus.width}, y : ${cactus.y + cactus.height}`
+                document.getElementById('tit').innerHTML = cactusPosition;
+            }
+        });
     
-        if (this.jumping) {
-            dino.y--;
-            this.jumpTimer++;
+        if (jumping) {
+            dino.y -= speed * 2;
+            jumpTimer += speed;
         } else {
-            if(dino.y < 200) {
-                dino.y++;
-                this.jumpTimer = 0;
+            if(dino.y < 400) {
+                dino.y += speed * 2;
+                jumpTimer = 0;
             }
         }
-        if(this.jumpTimer > 100) {
-            this.jumping = false;
+        if(jumpTimer > 100) {
+            jumping = false;
         }
         
         dino.draw();
-        setTimeout(() => {
-            
-            this.endGame();
-        }, 2000);
-        
-        return animation = requestAnimationFrame(game);
-    },
-    isCrashed(dino, cactus){
-        var xNum = cactus.x - (dino.x + dino.width);
-        var yNum = cactus.y - (dino.y + dino.height);
-        if (xNum < 0 && yNum < 0) {
-            this.endGame();
+    };
+    function isCrashed(dino, cactus){
+        var xNum = (cactus.x + cactus.width) - (dino.x + dino.width);
+        var yNum = (cactus.y + cactus.height) - (dino.y + dino.height);
+        diffPosition = `장애물 - 나의 위치 x : ${xNum}, y : ${yNum}`
+        document.getElementById('diff').innerHTML = diffPosition;
+
+        if (xNum < -cactus.width && yNum <= cactus.height) {
+            console.log(xNum, yNum)
+            finishGame();
         }
-    },
-    endGame(){
-        console.log(animation);
+    };
+    function dieAction(){
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        cancelAnimationFrame(this.animation);
-        this.isEnd = true;
+        dieAnimation = requestAnimationFrame(dieAction);
+        diffPosition = '진짜 못하시네요... 실망입니다. - 당신의 컴퓨터';
+        document.getElementById('diff').innerHTML = diffPosition;
+    
+        if (dying) {
+            if (dieTimer > 500) {
+                dieTimer = 0;
+                dying = false;
+                cancelAnimationFrame(dieAnimation);
+                return false;
+            } else if (dieTimer > 100) {
+                dieTimer += speed * 2;
+                dino.y += speed * 2;
+            } else {
+                dieTimer += speed * 2;
+                dino.y -= speed * 2;
+            }
+        }
+
+        dino.draw();
+    };
+    function finishGame(){
+        cancelAnimationFrame(animation);
+        dying = true;
+        dieAction();
+        console.log('end');
+        document.getElementsByTagName('body')[0].classList.add('over');
+        isEnd = true;
     }
+    function reStartGame(){
+        document.getElementsByTagName('body')[0].classList.remove('over');
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        animation = null;
+        dieAnimation = null;
+        timer = 0;
+        cactusArray = [];
+        jumping = false;
+        jumpTimer = 0;
+        isEnd = false;
+        dino.x = 0;
+        dino.y = 400;
+        startGame();
+    }
+
+    startGame();
+    document.addEventListener('keydown', function(e){
+        if (e.code === 'Space') {
+            if (isEnd) {
+                reStartGame();
+            } else {
+                if (!jumping) {
+                    jumping = true;
+                }
+            }
+        }
+    })
 }
 
 
-document.addEventListener('keydown', function(e){
-    if (e.code === 'Space') {
-        gameState.jumping = true;
-    }
-})
+
+window.onload = function(){
+    gameState();
+}
